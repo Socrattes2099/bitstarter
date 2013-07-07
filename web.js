39c169dae4af9e07bf1cb5aebe869a7152a43623
Bayron Guevara <bayron.guevara@gmail.com>
2013-07-06 21:25:50 -0600

    read contents from index.html file

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var fileContents = fs.readFileSync('index.html');	
+  var fileContents = fs.readFileSync('index.html').toString();	
   response.send(fileContents);
 });
 
