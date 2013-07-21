715df171eaf45672963eb27945630dddbb478284
Bayron Guevara <bayron.guevara@gmail.com>
2013-07-21 02:03:17 -0600

    - Change default port to 8080

--- grader.js
+++ grader.js
@@ -3,6 +3,7 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -17,16 +18,12 @@ var assertFileExists = function(infile){
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile){
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
-
 var loadChecks = function(checksfile){
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile){
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlString = function(htmlstring, checksfile){
+    $ = cheerio.load(htmlstring);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks){
@@ -35,21 +32,47 @@ var checkHtmlFile = function(htmlfile, checksfile){
     }
 
     return out;
-}
+};
+
+var checkUrlResponse = function(program){
+    return function(result, response){
+        if ( result instanceof Error ){
+            var jsonError = {
+                error: "Cannot download file from url"
+            };
+            console.log( JSON.stringify(jsonError) );
+        } else {
+            //console.log(result);
+            checkAndPrintOutput(program, result);
+        }
+    }
+};
 
 var clone = function(fn){
     return fn.bind({});
+};
+
+var checkAndPrintOutput = function(program, htmlstring){
+    var checkJson = checkHtmlString(htmlstring, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
 }
 
 if(require.main == module){
     program
         .option('-c --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to index.html', null, null)
         .parse(process.argv);
 
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url)
+        rest.get(program.url).on('complete', checkUrlResponse(program));
+    else {
+        var htmlstring = fs.readFileSync(program.file).toString();
+        checkAndPrintOutput(program, htmlstring);
+    }
+
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
